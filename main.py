46f3c7fa18f81494a2a7030dc13c5d8d19d25921
Dillon-Mendoza <Dm.auggie@gmail.com>
2025-06-04 18:20:50 -0600

    Scraped to a site, then was captcha blocked

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 import time
@@ -8,6 +9,9 @@ service = Service(executable_path='chromedriver')
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://www.google.com")
+
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.send_keys("Github" + Keys.ENTER)
 
 time.sleep(10)
 
